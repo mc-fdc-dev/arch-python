f299579326cbe03fa42bd935f2880b48a090093d
mc_fdc <Masato.04.11.2007@gmail.com>
2023-04-08 17:23:24 +0000

    funy

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ PYTHON_VERSION=3.11.1
 
 mkdir /tmp/python
 cd /tmp/python
-wget -o python.tar.xz https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
-tar xJf python.tar.xz
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
+tar xJf Python-$PYTHON_VERSION.tar.xz
 cd Python-$PYTHON_VERSION
 ./configure --enable-optimizations
 make
